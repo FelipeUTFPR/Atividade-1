aaec7f748516d392274f0947fef979770418985e
FelipeUTFPR <fps2887@gmail.com>
2021-11-12 02:14:37 -0300

    teste falhando

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -42,4 +42,44 @@ describe('Filtro', () => {
 
       )
   })
+
+  test('Para Valores de Faturas entre 2000 e 2500 e dif datafat 30 dias', () => {
+    expect(
+        f.filtro([{
+            codigo: 1,
+            valor: 2000,
+            dataFatura: '2021-11-08',
+            cliente: {
+              nome: 'Felipe',
+              dataInclusao: '2021-03-04',
+              estado: 'SC'
+            }
+          },
+          {
+            codigo: 2,
+            valor: 2000,
+            dataFatura: '2021-09-10',
+            cliente: {
+              nome: 'João Paulo',
+              dataInclusao: '2021-06-01',
+              estado: 'SC'
+            }
+          },
+          {
+            codigo: 3,
+            valor: 2400,
+            dataFatura: '2021-11-08',
+            cliente: {
+              nome: 'Fernanda',
+              dataInclusao: '2021-05-01',
+              estado: 'SC'
+            }
+          }
+        ])
+      )
+      .toEqual([{"cliente": {"dataInclusao": "2021-03-04", "estado": "SC", "nome": "Felipe"}, "codigo": 1, "dataFatura": "2021-11-08", "valor": 2000},]
+
+
+      )
+  })
 })
